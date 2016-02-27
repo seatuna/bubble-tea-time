23f9e7115c74268daffe031fa045fdeacae0e9e0
Celena T <chibiynm@gmail.com>
2016-02-27 18:44:03 -0500

    Add handlebars file for drinks
    All drinks display on page upon loading

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,30 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+let displayDrinks = function(response){
+  console.log('displayDrinks works');
+  let drinks = response.drinks;
+  let drinkListing = require('./iterate-drinks.handlebars');
+    $('.content').html(drinkListing({drinks
+    }));
+};
+
+let getDrinks = function(){
+  $.ajax({
+    url: "http://localhost:3000/drinks",
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(drinks){
+    displayDrinks(drinks);
+    console.log('getDrinks work');
+  });
+};
+
+// $('.books').on("click", function() {
+//   getBooks();
+// });
+
+$(document).ready(function(){
+  getDrinks();
+});
