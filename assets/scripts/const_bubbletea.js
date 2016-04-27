8d38e821a2a65ce07be7972cfa189444f465ec05
Celena T <chibiynm@gmail.com>
2016-04-27 01:05:24 -0400

    Display comments and add textbox

--- assets/scripts/const_bubbletea.js
+++ assets/scripts/const_bubbletea.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const bbtApp = {
-  baseUrl: 'https://tranquil-basin-77065.herokuapp.com'
+  // baseUrl: 'https://tranquil-basin-77065.herokuapp.com'
+  baseUrl: 'http://localhost:3000'
 };
 
 module.exports = {
